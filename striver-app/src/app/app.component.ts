2f0f1f0587ce3338a6c5d17f6a0c652acc52af8f
Zulifqar Ahmad <zulifqar.ahmad@hotmail.com>
2019-11-28 01:16:47 -0600

    code changes for basic application infrastructure

--- striver-app/src/app/app.component.ts
+++ striver-app/src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
-
+import { NbSidebarService } from '@nebular/theme';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'striver-app';
+  menu: any[] = [
+    {
+      title: 'Home',
+      icon: 'home-outline',
+      link: '/home',
+      home: true,
+      children: undefined
+    },
+    {
+      title: 'Users',
+      icon: 'people-outline',
+      link: '/users',
+      home: false,
+      children: undefined
+    },
+    {
+      title: 'Keys',
+      icon: 'keypad-outline',
+      link: '/users',
+      home: false,
+      children: undefined
+    }
+  ];
+  constructor(private sidebarService: NbSidebarService) {}
+
+  toggle() {
+    this.sidebarService.toggle(true);
+    return false;
+  }
 }
--- striver-app/src/app/app.module.ts
+++ striver-app/src/app/app.module.ts
@@ -3,16 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbMenuModule,
+  NbButtonModule,
+  NbContextMenuModule,
+  NbSidebarModule,
+  NbSidebarService,
+  NbCardModule
+} from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, UsersComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbMenuModule.forRoot(),
+    NbContextMenuModule,
+    NbButtonModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbSidebarModule,
+    NbEvaIconsModule,
+    NbCardModule
   ],
-  providers: [],
+  providers: [NbSidebarService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
